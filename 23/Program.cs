445aa81e83c96a6c1680395a60bb8682ceb4600b
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-20 13:17:48 +0300

    23

---
+++ 23/Program.cs
@@ -0,0 +1,18 @@
+﻿// 23Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите число N");
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+void Kub(int n)
+{  if(x<0){
+    System.Console.WriteLine("не то число");
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"{i}|{Math.Pow(i,3)}");
+    }
+}
+
+
+Kub(x);
