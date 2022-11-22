a8d56e82579c4014cecb2b236ef3e307af81615f
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-22 12:37:20 +0300

    30

---
+++ 30/Program.cs
@@ -0,0 +1,22 @@
+﻿//Заполнить массив случайными числами 
+
+int[]array = new int[8];
+
+void RandArray(int[]arr){
+
+Random rnd = new Random();    
+for(int i= 0; i<arr.Length;i++)
+{
+array[i] = rnd.Next(0,2);
+
+}
+}
+void PrintArrau(int[]arr){
+   for(int i= 0; i<arr.Length;i++)
+   { 
+    Console.Write($"{array[i]} ");
+   }
+}
+RandArray(array);
+PrintArrau(array);
+
