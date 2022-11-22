ac212de15ec0233d88361a2bd3569877c4ad70df
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-22 11:29:44 +0300

    24

---
+++ 24/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу,которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+void Sum(int n)
+{  int sum =0;
+ if(n<0){
+     System.Console.WriteLine("не то число");
+     }else{
+    for (int i = 1; i <= n; i++)
+    {
+       sum=sum+i;
+    } Console.WriteLine($"сумма последовательности {sum}");}
+//return sum;
+}
+
+Sum(number);
