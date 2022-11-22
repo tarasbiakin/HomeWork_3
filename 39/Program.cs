44b3de3a5f19def8a5d5e5cb3558618f341d5c05
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-22 09:49:04 +0300

    округления и периметр

--- 39/Program.cs
+++ 39/Program.cs
@@ -17,9 +17,9 @@ double xc  = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координаты точки 3 YC: ");
 double yc = Convert.ToDouble(Console.ReadLine());
 
-double st1=Math.Sqrt(((xa-xb)*(xa-xb))+((ya-yb)*(ya-yb)));//Math.Pow(i,3)
-double st2=Math.Sqrt(((xa-xc)*(xa-xc))+((ya-yc)*(ya-yc)));
-double st3=Math.Sqrt(((xb-xc)*(xb-xc))+((yb-yc)*(ya-yc)));
+double st1=Math.Sqrt(Math.Pow (xa-xb,2)+(Math.Pow(ya-yb,2)));//Math.Pow(i,3)
+double st2=Math.Sqrt(Math.Pow (xa-xc, 2))+(Math.Pow (ya-yc,2));
+double st3=Math.Sqrt((Math.Pow (xb-xc,2))+(Math.Pow (yb-yc,2)));
 //double p = st1+st2+st3;
 
 
@@ -28,18 +28,24 @@ double Lenght (double xa, double ya,double xb, double yb,double xc,double yc){
     return res;
 }
 
-double Sqare (double st1, double st2,double st3)
+double Sqare (double st1, double st2,double st3,double Lenght)
 {
-    double res = Math.Sqrt(((st1+st2+st3)/2)*(((st1+st2+st3)/2)-st1)*(((st1+st2+st3)/2)-st2)*(((st1+st2+st3)/2)-st3));
+    double p = Lenght/2;
+    double res = Math.Sqrt(p*((p-st1)*(p-st2)*(p-st3)));
     return res;
+    //return p;
 }
 
 double lenght = Lenght(xa,ya,xb,yb,xc,yc);
 double lenghtRound= Math.Round(lenght,2, MidpointRounding.ToZero);
 Console.WriteLine("Периметр:"+lenghtRound);
-//Console.WriteLine($" {st1},{st2},{st3}");
+Console.WriteLine($" {Math.Round(st1,3,MidpointRounding.ToZero)}| {Math.Round(st2,3,MidpointRounding.ToZero)}| {Math.Round(st3,3,MidpointRounding.ToZero)}");
 
 
-double sqr = Sqare (st1,st2,st3);
-double sqrRound= Math.Round(sqr,2, MidpointRounding.ToZero);
+double sqr = Sqare (st1,st2,st3,lenght);
+double sqrRound= Math.Round(sqr,4, MidpointRounding.ToZero);
 Console.WriteLine("Площадь: " +sqrRound);
+
+//  double per = Sqare (st1,st2,st3);
+//  double sqrRound= Math.Round(per,2, MidpointRounding.ToZero);
+//  Console.WriteLine("Площадь: " +sqrRound);
